86dddd28d32c57e784d8692a733a6b4669fe0d20
Xinyu <xinyuyan605@gmail.com>
2024-10-24 21:51:40 +0800

    feat: policyholder filter

--- app/policyholders/page.tsx
+++ app/policyholders/page.tsx
@@ -1,8 +1,23 @@
-import { type FC } from 'react';
+'use client';
+
+import { useCallback, useState, type FC } from 'react';
+
+import PolicyholderIdFilter from '@/app/policyholders/components/PolicyholderIdFilter';
 
 const Policyholders: FC = () => {
+  const [, setKeyword] = useState('');
+
+  const handleSearchClient = useCallback((newKeyword: string) => {
+    setKeyword(newKeyword);
+  }, []);
+
   return (
-    <div>policyholders{/* TODO: add policyholder organization modules  */}</div>
+    <div className="flex flex-col gap-4 p-6">
+      <h2 className="pb-2 text-2xl">保戶關係查詢</h2>
+      <PolicyholderIdFilter onSearch={handleSearchClient} />
+      <h2 className="pt-2 text-2xl">關係圖</h2>
+      <div>{/* TODO: add policyholder organization modules  */}</div>
+    </div>
   );
 };
 
